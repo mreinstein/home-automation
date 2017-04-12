b0cc50dbd5942f09abbc94a30e5bc94faf2cca05
Mike Reinstein <reinstein.mike@gmail.com>
2017-04-12 15:17:16 -0700

    tweaks

--- prototype/index.js
+++ prototype/index.js
@@ -1,7 +1,5 @@
 'use strict'
 
-//const Detector = require('snowboy').Detector
-//const Models   = require('snowboy').Models
 const STT      = require('watson-developer-cloud/speech-to-text/v1')
 const dotenv   = require('dotenv').config()
 const lifx     = require('node-lifx').Client
@@ -10,6 +8,104 @@ const snowboy  = require('./lib/snowboy')
 const state    = require('./lib/finite-state-machine')
 const tts      = require('./lib/tts')
 
+
+function idleState() {
+  //let mic
+
+  detector.on('error', function (er) {
+    console.log('snowboy error', er)
+  })
+
+  detector.on('hotword', async function(index, hotword) {
+    console.log('hotword', index, hotword)
+    fsm.setState('LISTENING')
+  })
+
+  let enter = function() {
+    record.start({
+      threshold: 0,
+      verbose: true
+    }).pipe(detector)
+  }
+
+  let exit = function() {
+    record.stop()
+    //mic.unpipe(detector)
+  }
+
+  return Object.freeze({ enter, exit })
+}
+
+
+function recordingState() {
+  let recognizerStream, text, mic
+
+  const speech_to_text = new STT({
+    username: process.env.WATSON_USERNAME,
+    password: process.env.WATSON_PASSWORD
+  })
+
+  let enter = async function() {
+    recognizerStream = speech_to_text.createRecognizeStream({ content_type: 'audio/l16; rate=16000', continuous: true, inactivity_timeout: 2 })
+
+    recognizerStream.on('error', function(event) {
+      //console.log('er', event)
+    })
+
+    recognizerStream.on('close', async function(event) {
+      console.log('\nWatson speech socket closed')
+
+      fsm.setState('IDLE')
+    })
+
+    recognizerStream.on('data', _processInput)
+
+    record.start({
+      threshold: 0,
+      verbose: false
+    }).pipe(recognizerStream)
+  }
+
+  let exit = function() {
+    record.stop()
+    recognizerStream.removeListener('data', _processInput)
+    //mic.unpipe(recognizerStream)
+    recognizerStream = undefined
+  }
+
+  let _processInput = function(data) {
+    data = data.toString().trim().toUpperCase()
+    if(data.length === 0) return
+
+    console.log('command:', data)
+
+    if(lights.length === 0) return
+
+    if(data.indexOf('EVENING LIGHT') > -1) {
+      setColor(lights[0], 29, 100, 50, 3500, 1000)
+    } else if(data.indexOf('READING LIGHT') > -1) {
+      setColor(lights[0], 0, 0, 65, 2500, 1000)
+    } else if(isCommand(data, morning_commands)) {
+      setColor(lights[0], 202, 100, 40, 3500, 60000)
+    } else if(isCommand(data, off_commands)) {
+      console.log('lights off')
+      lights[0].off(600)
+    } else if (isCommand(data, on_commands)) {
+      lights[0].on(1200)
+    } else if(data === 'LIGHT' || data === 'LIGHTS' || data === 'LET\'S' || data === 'LETS') {
+      toggleLight(lights[0], 800)
+    }
+  }
+
+  return Object.freeze({ enter, exit })
+}
+
+
+async function sleep(ms) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(resolve, ms)
+  })
+}
 
 function isCommand(input, commands) {
   for(let i=0; i < commands.length; i++) {
@@ -46,46 +142,6 @@ function toggleLight(light, duration=0) {
 }
 
 
-function idleState() {
-  let mic
-
-  /*
-  const detector = new Detector({
-    resource: __dirname + '/resources/common.res',
-    models: models,
-
-    // audioGain controls whether to increase (>1) or decrease (<1) input volume.
-    audioGain: 1.0
-  })
-  */
-
-  detector.on('error', function (er) {
-    console.log('snowboy error', er)
-  })
-
-  detector.on('hotword', async function(index, hotword) {
-    console.log('hotword', index, hotword)
-    fsm.setState('LISTENING')
-  })
-
-  let enter = function() {
-    mic = record.start({
-      threshold: 0,
-      verbose: false
-    })
-
-    mic.pipe(detector)
-  }
-
-  let exit = function() {
-    record.stop()
-    mic.unpipe(detector)
-  }
-
-  return Object.freeze({ enter, exit })
-}
-
-
 function listeningState() {
   let enter = async function() {
     const choices = [ 'acknowledged', 'at your service', 'Hiya' ]
@@ -103,94 +159,8 @@ function listeningState() {
 }
 
 
-function recordingState() {
-  let recognizerStream, text, mic
-
-  const speech_to_text = new STT({
-    username: process.env.WATSON_USERNAME,
-    password: process.env.WATSON_PASSWORD
-  })
-
-  let enter = async function() {
-    mic = record.start({
-      threshold: 0,
-      verbose: false
-    })
-
-    recognizerStream = speech_to_text.createRecognizeStream({ content_type: 'audio/l16; rate=16000', continuous: true, inactivity_timeout: 2 })
-
-    recognizerStream.on('error', function(event) {
-      //console.log('er', event)
-    })
-
-    recognizerStream.on('close', async function(event) {
-      console.log('\nWatson speech socket closed')
-
-      fsm.setState('IDLE')
-    })
-
-    recognizerStream.on('data', _processInput)
-    mic.pipe(recognizerStream)
-  }
-
-  let exit = function() {
-    recognizerStream.removeListener('data', _processInput)
-    record.stop()
-    mic.unpipe(recognizerStream)
-    recognizerStream = undefined
-  }
-
-  let _processInput = function(data) {
-    data = data.toString().trim().toUpperCase()
-    if(data.length === 0) return
-
-    console.log('command:', data)
-
-    if(lights.length === 0) return
-
-    if(data.indexOf('EVENING LIGHT') > -1) {
-      setColor(lights[0], 29, 100, 50, 3500, 1000)
-    } else if(data.indexOf('READING LIGHT') > -1) {
-      setColor(lights[0], 0, 0, 65, 2500, 1000)
-    } else if(isCommand(data, morning_commands)) {
-      setColor(lights[0], 202, 100, 40, 3500, 60000)
-    } else if(isCommand(data, off_commands)) {
-      console.log('lights off')
-      lights[0].off(600)
-    } else if (isCommand(data, on_commands)) {
-      lights[0].on(1200)
-    } else if(data === 'LIGHT' || data === 'LIGHTS' || data === 'LET\'S' || data === 'LETS') {
-      toggleLight(lights[0], 800)
-    }
-  }
-
-  return Object.freeze({ enter, exit })
-}
-
-
 // playing a shoutcast station
 //mpg123 -C http://206.190.150.90:8301/stream
-
-/*
-const models = new Models()
-
-models.add({
-  file: __dirname + '/resources/stanley-mike-pi.pmdl',
-
-  // Detection sensitivity controls how sensitive the detection is.
-  // It is a value between 0 and 1. Increasing the sensitivity value
-  // leads to better detection rate, but also higher false alarm rate.
-  // It is an important parameter that you should play with in your
-  sensitivity: '0.5',
-  hotwords : 'stanley'
-})
-
-models.add({
-  file: __dirname + '/resources/stanley-heather-pi.pmdl',
-  sensitivity: '0.5',
-  hotwords : 'stanley'
-})
-*/
 
 const detector = snowboy()
 
