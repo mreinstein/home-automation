15b381ff775354b01c5ac3d47c9aea20ae9b1e5e
Mike Reinstein <reinstein.mike@gmail.com>
2017-04-15 03:40:27 -0700

    minor tweaks

--- chromecasting/test2.js
+++ chromecasting/test2.js
@@ -1,37 +1,38 @@
-var ChromecastAPI = require('chromecast-api')
- 
- 
-var browser = new ChromecastAPI.Browser()
- 
+'use strict'
+
+const ChromecastAPI = require('chromecast-api')
+
+
+const browser = new ChromecastAPI.Browser()
+
 browser.on('deviceOn', function (device) {
-    //var urlMedia = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4';
-    var urlMedia = 'http://192.168.42.66:8000/La-La-Land.mp4';
- 
-    device.play(urlMedia, 0, function () {
-        console.log('Playing in your chromecast')
- 
-        /*
-        setTimeout(function () {
-            //Pause the video 
-            device.pause(function () {
-                console.log('Paused')
-            })
-        }, 20000)
- 
-        setTimeout(function () {
-            //Stop video 
-            device.stop(function () {
-                console.log('Stopped')
-            })
-        }, 30000)
- 
-        setTimeout(function () {
-            //Close the streaming 
-            device.close(function () {
-                console.log('Closed')
-            })
-        }, 40000)
-        */
-    })
+  //const urlMedia = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4'
+  const urlMedia = 'http://192.168.42.66:8000/La-La-Land.mp4'
+
+  device.play(urlMedia, 0, function() {
+    console.log('Playing in your chromecast')
+
+    /*
+    setTimeout(function () {
+      //Pause the video
+      device.pause(function () {
+        console.log('Paused')
+      })
+    }, 20000)
+
+    setTimeout(function () {
+      //Stop video
+      device.stop(function () {
+        console.log('Stopped')
+      })
+    }, 30000)
+
+    setTimeout(function () {
+      //Close the streaming
+      device.close(function () {
+        console.log('Closed')
+      })
+    }, 40000)
+    */
+  })
 })
- 
